52e1317355c2913d79b9e50a2cd5c8d464044d54
陈黎明 <2360187899@qq.com>
2026-02-21 19:47:53 +0800

    feat(file): 集成Nacos服务发现和配置中心
    - 添加@EnableDiscoveryClient注解启用服务发现
    - 配置bootstrap.yml文件集成Nacos注册中心和服务配置
    - 引入Spring Cloud Alibaba Nacos依赖包
    - 添加Druid数据库连接池支持
    - 集成MySQL数据库驱动和MyBatis-Plus框架
    - 配置Nacos服务器地址和认证信息

--- soul-frame-modules/soul-frame-modules-file/src/main/java/com/clm/modules/file/SoulFrameModulesFileApplication.java
+++ soul-frame-modules/soul-frame-modules-file/src/main/java/com/clm/modules/file/SoulFrameModulesFileApplication.java
@@ -2,7 +2,10 @@ package com.clm.modules.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SoulFrameModulesFileApplication {
 
